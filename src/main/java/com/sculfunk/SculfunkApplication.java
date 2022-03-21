8e741f3f99f3aed7f7414eebfbe0e5af91c4d755
김민혁 <94831670+kmh916@users.noreply.github.com>
2022-03-21 14:38:15 +0900

    initial commit

---
+++ src/main/java/com/sculfunk/SculfunkApplication.java
@@ -0,0 +1,13 @@
+package com.sculfunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SculfunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SculfunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sculfunk/SculfunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sculfunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SculfunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
